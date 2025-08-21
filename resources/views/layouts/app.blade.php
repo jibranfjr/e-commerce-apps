f5cbe5c6bbc23b6074b3055281019bb9f3df5e63
Jibran Fajar <jibran.fjr13@gmail.com>
2025-08-21 21:07:32 +0800

    memindahkan asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Toko Permata Puri Bali</title>
-    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/style-v2.css') }}">
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style-v2.css') }}">
 </head>
 <body>
 
@@ -16,7 +16,7 @@
 
     @include('partials.footer')
 
-    <script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/fontawesome/js/all.min.js') }}"></script>
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('fontawesome/js/all.min.js') }}"></script>
 </body>
 </html>
