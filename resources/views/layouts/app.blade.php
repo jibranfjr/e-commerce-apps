2f9c5217208c66b74bcc6f1c40ea703fe0925c38
Jibran Fajar <jibran.fjr13@gmail.com>
2025-08-21 20:56:40 +0800

    memindahkan aset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Toko Permata Puri Bali</title>
-    <!-- Favicon default -->
-<link rel="icon" href="{{ asset('image/logo.png') }}" type="image/png">
-
-<!-- Untuk browser yang minta .ico -->
-<link rel="shortcut icon" href="{{ asset('image/logo.ico') }}" type="image/x-icon">
-
-<!-- Untuk Apple/Android -->
-<link rel="apple-touch-icon" href="{{ asset('image/logo.png') }}">
-    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style-v2.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style-v2.css') }}">
 </head>
 <body>
 
@@ -24,7 +16,7 @@
 
     @include('partials.footer')
 
-    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('fontawesome/js/all.min.js') }}"></script>
+    <script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/fontawesome/js/all.min.js') }}"></script>
 </body>
 </html>
